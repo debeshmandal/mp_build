d2db5bbe79ae504404809ac2095ab0a277bf7216
Debesh Mandal <debesh.mandal@nanograb.com>
2025-04-14 20:14:04 +0100

    Add windows error handling in run

--- mp_build/job.py
+++ mp_build/job.py
@@ -1,9 +1,14 @@
 import multiprocessing as mp
+import os
+from softnanotools.logger import Logger
+logger = Logger(__name__)
 
 def job(x):
     return x * x
 
 def run():
+    if os.name == 'nt':
+        logger.error("Windows is not supported", exc_info=True)
     results = []
     with mp.get_context("fork").Pool(mp.cpu_count()) as pool:
         results = pool.map(job, range(10))
